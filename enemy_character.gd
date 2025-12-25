85f11aa0395505dffd96fdaf9ae80c5311819bd4
Solomon Firfer <theepicsuper212@gmail.com>
2025-12-25 12:58:36 -0500

    did minor refactoring

--- enemy_character.gd
+++ enemy_character.gd
@@ -5,16 +5,14 @@ extends CharacterBody2D
 const SPEED = 500.0
 
 var player: Node2D
-var player_character: CharacterBody2D
 
 func _ready() -> void:
 	player = get_tree().current_scene.find_child("Player") as Node2D
-	player_character = player.find_child("Character") as CharacterBody2D
-	navigation_agent_2d.target_position = player_character.position
+	navigation_agent_2d.target_position = player.position
 
 func _physics_process(delta: float) -> void:
 	if navigation_agent_2d.is_target_reached():
-		navigation_agent_2d.target_position = player_character.position
+		navigation_agent_2d.target_position = player.position
 	else:
 		var nav_point_direction = to_local(navigation_agent_2d.get_next_path_position())
 	
